7cdb0f395380b5ef6e3c923ca8b7eadd63b44d59
Anthony Caron <antcar1992@gmail.com>
2020-12-12 10:48:54 -0800

    Added property source & config bean

--- src/main/java/com/antcar/sfgdi/SfgDiApplication.java
+++ src/main/java/com/antcar/sfgdi/SfgDiApplication.java
@@ -1,10 +1,7 @@
 package com.antcar.sfgdi;
 
-import com.antcar.sfgdi.controllers.ConstructorInjectedController;
 import com.antcar.sfgdi.controllers.I18nController;
-import com.antcar.sfgdi.controllers.MyController;
-import com.antcar.sfgdi.controllers.PropertyInjectedController;
-import com.antcar.sfgdi.controllers.SetterInjectedController;
+import com.antcar.sfgdi.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -18,21 +15,11 @@ public class SfgDiApplication {
 		I18nController i18nController = (I18nController) ctx.getBean("i18nController");
 		System.out.println(i18nController.sayHello());
 
-		System.out.println("----- Primary Bean");
-		MyController myController = (MyController) ctx.getBean("myController");
-		System.out.println(myController.sayHello());
+		FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
+		System.out.println(fakeDataSource.getUser());
+		System.out.println(fakeDataSource.getPassword());
+		System.out.println(fakeDataSource.getUrl());
 
-		System.out.println("----- Property");
-		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
-		System.out.println(propertyInjectedController.getGreeting());
-
-		System.out.println("----- Setter");
-		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
-		System.out.println(setterInjectedController.getGreeting());
-
-		System.out.println("----- Constructor");
-		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
-		System.out.println(constructorInjectedController.getGreeting());
 	}
 
 }
